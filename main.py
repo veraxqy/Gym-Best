e25475c8b2fd492bdd162e719947cdf2c92aed07
veraxqy <elderbruno2014@gmail.com>
2024-10-03 21:15:01 -0300

    Adicionando configurações iniciasi no main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flet import *
 
 def main(page: Page):
-    pass
+    page.title = "Gym-Best"
+    page.horizontal_alignment = "center"
+    page.scroll = "adaptive"
+
+    page.add()
 
 app(target=main)
