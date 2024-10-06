af6321ee45665ae0243961035ff84b043f207498
veraxqy <elderbruno2014@gmail.com>
2024-10-05 23:58:37 -0300

    Criação da class Exercicios no main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from flet import *
 
+class Exercicios(UserControl):
+    def __init__(self, nome, carga, repeticao, status_exercicio, delete_exercicio, show_checkbox):
+        super().__init__()
+        self.completed = False
+        self.nome = nome
+        self.carga = carga
+        self.repeticao = repeticao
+        self.status_exercicio = status_exercicio
+        self.delete_exercicio = delete_exercicio
+        self.show_checkbox = show_checkbox
+
 def main(page: Page):
     page.title = "Gym-Best"
     page.horizontal_alignment = "center"
