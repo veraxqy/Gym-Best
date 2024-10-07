310fb8447d18f37c27f8cdce6db2dd04aba68406
veraxqy <elderbruno2014@gmail.com>
2024-10-06 22:23:41 -0300

    Criação do display_view e edit_view da class Exercicios no main.py

--- main.py
+++ main.py
@@ -10,6 +10,54 @@ class Exercicios(UserControl):
         self.status_exercicio = status_exercicio
         self.delete_exercicio = delete_exercicio
         self.show_checkbox = show_checkbox
+    
+    def build(self):
+        self.check_exercicio = Checkbox(
+            value = False, label = self.nome, on_change = self.status_changed, visible=self.show_checkbox
+        )
+
+        self.edit_nome = TextField(expand=1)
+        self.edit_carga = TextField(expand=1)
+        self.edit_repeticao = TextField(expand=1)
+        self.display_view = Row(
+            alignment="spaceBetween",
+            vertical_alignment="center",
+            controls=[
+                self.check_exercicio if self.show_checkbox else Text(value=self.nome),
+                Row(
+                    spacing=0,
+                    controls=[
+                        IconButton(
+                            icon=icons.CREATE_OUTLINED,
+                            tooltip="Editar Exercício",
+                            on_click=self.edit_exercicio,
+                            icon_color=colors.WHITE,
+                        ),
+                        IconButton(
+                            icon=icons.DELETE_OUTLINED,
+                            tooltip="Deletar Exercício",
+                            on_click=self.delet_exercicio,
+                            icon_color=colors.WHITE,
+                        )
+                    ]
+                )
+            ]
+        )
+        self.edit_view = Row(
+            visible=False,
+            alignment="spaceBetween",
+            vertical_alignment="center",
+            controls=[
+                self.edit_nome,
+                IconButton(
+                    icon=icons.DONE_OUTLINE_OUTLINED,
+                    icon_color=colors.WHITE,
+                    tooltip="Atualizar Exercício",
+                    on_click=self.save_exercicio,
+                )
+            ]
+        )
+        return super().build()
 
 def main(page: Page):
     page.title = "Gym-Best"
